8341c3f4513fa0d9b306a79bd918ea0a0355a972
unknown <Alexey@.(none)>
2010-02-03 16:32:11 -0700

    Fixed incorrect resource header reference. Fixed the path to StdAfx.h. Reformatted Tools.cpp a little.

--- src/client/src/Tools.cpp
+++ src/client/src/Tools.cpp
@@ -7,6 +7,8 @@
 using namespace Tools;
 using namespace std;
 
+// Based on Raymond Chen's implementation.
+// http://blogs.msdn.com/oldnewthing/archive/2004/01/30/65013.aspx
 LPCWSTR GetStringResource(HINSTANCE instance, UINT id)
 {
 	// Convert the string ID into a bundle number
@@ -31,6 +33,10 @@ wstring Tools::LoadStringResource(HINSTANCE instance, UINT id)
 	LPCWSTR resource = GetStringResource(instance, id);
 	wstring str;
 	str.reserve(1 + (resource ? *resource : 0));
-	copy(resource + 1, resource + 1 + str.length(), std::back_insert_iterator<wstring>(str));
+	copy
+		( resource + 1
+		, resource + 1 + str.length()
+		, std::back_insert_iterator<wstring>(str)
+		);
 	return str;
 }
