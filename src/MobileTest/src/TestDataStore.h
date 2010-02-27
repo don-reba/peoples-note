e65e598f5216ebe419c740922479242e90a9c18a
unknown <Alexey@.(none)>
2010-02-27 13:53:38 -0700

    Corrected the DataStore loading test.

--- src/MobileTest/src/TestDataStore.h
+++ src/MobileTest/src/TestDataStore.h
@@ -6,10 +6,17 @@ void TestDataStoreLoad(Test & test)
 {
 	DataStore store(L"Program Files\\MobileTest");
 
+	::DeleteFile(L"Program Files\\MobileTest\\test.db");
+	
+	TEST_CHECK_EQUAL
+		( GetFileAttributes(L"Program Files\\MobileTest\\test.db")
+		, 0xFFFFFFFF
+		);
+
 	store.LoadOrCreate(L"test");
 
 	TEST_CHECK_NOT_EQUAL
-		( GetFileAttributes(L"Program Files\\MobileTest\\test.db")
+		( ::GetFileAttributes(L"Program Files\\MobileTest\\test.db")
 		, 0xFFFFFFFF
 		);
 }
