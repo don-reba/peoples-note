37d66701473de24172d0d20f5c2e28f818e54d03
Alexey Badalov <don_reba@inbox.ru>
2010-09-06 15:49:07 -0600

    Updated Codeplex scripts. Wrote a readme document for other developers.

---
+++ doc/readme.txt
@@ -0,0 +1,35 @@
+-----------------
+Project Structure
+-----------------
+
+doc
+	The project's documentation.
+
+gfx
+	Graphics asset sources.
+
+"src\client"
+	Source code for the main People's Note client project.
+
+"src\MobileTest"
+	Those unit tests for Peope's Note that need to run in a WinCE environment.
+
+"src\Test"
+	Those unit tests for People's Note that can be run in a Win32 environment.
+
+"src\CallDump"
+	Library used by the built-in profiler in People's Note.
+
+"src\HTMLayout"
+	Headers and binaries for the HTMLayout library used by People's Note.
+
+"src\Evernote API"
+	Win32/C++ port of the Evernote API and Apache Thrift.
+
+------------
+Requirements
+------------
+
+Visual Studio 2008
+Boost 1.41.0
+Inkscape 0.48
--- scripts/update Codeplex.cmd
+++ scripts/update Codeplex.cmd
@@ -4,14 +4,26 @@ pushd ..
 
 set dst=Codeplex
 set opt=/s /xf *.swp *.swo *.exe /njh /njs /nfl /ndl
+set winCEopt=/xd "Windows Mobile 6 Professional SDK (ARMV4I)" /xf *.user
+set win32opt=/xd Debug Release
 
 rmdir/s/q %dst%
 mkdir %dst%
 
-robocopy "src\client" "%dst%\src\client" %opt% /xd "Windows Mobile 6 Professional SDK (ARMV4I)" /xf *.user
+robocopy "src\client"       "%dst%\src\client"       %opt% %winCEopt%
+robocopy "src\MobileTest"   "%dst%\src\MobileTest"   %opt% %winCEopt%
+robocopy "src\Test"         "%dst%\src\Test"         %opt% %win32opt%
+robocopy "src\CallDump"     "%dst%\src\CallDump"     %opt% %winCEopt%
+robocopy "src\Evernote API" "%dst%\src\Evernote API" %opt% %winCEopt%
+robocopy "src\HTMLayout"    "%dst%\src\HTMLayout"    %opt%
+
+xcopy/q/y "src\Evernote client.sln" "%dst%\src"
+
+robocopy "gfx" "%dst%\gfx" *.svg %opt%
+robocopy "gfx\icons" "%dst%\gfx\icons" %opt%
 
 mkdir "%dst%\doc"
-xcopy/q/y "doc\thoughts.txt"     "%dst%\doc"
+xcopy/q/y "doc\readme.txt"       "%dst%\doc"
 xcopy/q/y "doc\user stories.txt" "%dst%\doc"
 
 popd
